7d22550462ceff9bc3ceb7855f24b4491f5dbb35
yamada-Syotaro <134474129+yamada-Syotaro@users.noreply.github.com>
2023-08-28 21:54:17 +0900

    ミノ消去

--- Assets/Mino.cs
+++ Assets/Mino.cs
@@ -15,6 +15,7 @@ public class Mino : MonoBehaviour
     // mino回転
     public Vector3 rotationPoint;
 
+    // grid
     private static Transform[,] grid = new Transform[width, height];
 
     void Update()
@@ -54,6 +55,8 @@ public class Mino : MonoBehaviour
             {
                 transform.position -= new Vector3(0, -1, 0);
                 AddToGrid();
+                // 今回の追加
+                CheckLines();
                 this.enabled = false;
                 FindObjectOfType<SpawnMino>().NewMino();
             }
@@ -67,8 +70,61 @@ public class Mino : MonoBehaviour
         }
     }
 
+    // 今回の追加 ラインがあるか？確認
+    public void CheckLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
+        {
+            if (HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+
+    // 今回の追加 列がそろっているか確認
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (grid[j, i] == null)
+                return false;
+        }
+        return true;
+    }
+
+    // 今回の追加 ラインを消す
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+
+    }
+
+    // 今回の追加 列を下げる
+    public void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (grid[j, y] != null)
+                {
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
     void AddToGrid()
     {
+
         foreach (Transform children in transform)
         {
             int roundX = Mathf.RoundToInt(children.transform.position.x);
@@ -76,8 +132,8 @@ public class Mino : MonoBehaviour
 
             grid[roundX, roundY] = children;
         }
-    }
 
+    }
 
     // minoの移動範囲の制御
     bool ValidMovement()
@@ -97,6 +153,7 @@ public class Mino : MonoBehaviour
             {
                 return false;
             }
+
 
         }
         return true;
